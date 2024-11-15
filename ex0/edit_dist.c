72c3c5292ecc30b3f23fabf359fb41d84bea298a
tom9798 <soustieltom@gmail.com>
2024-11-15 20:21:00 +0200

    first attempt

--- ex0/edit_dist.c
+++ ex0/edit_dist.c
@@ -19,5 +19,35 @@ int main() {
 }
 
 int edit_dist(char word1[], char word2[]) {
-	/* YOUR CODE HERE */
+    int len1 = strlen(word1);
+    int len2 = strlen(word2);
+
+    //set up a 2D array to store the edit distance
+    int dp[len1 + 1][len2 + 1];
+
+    //initialize the first row and column
+    for (int i = 0; i <= len1; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= len2; j++) {
+        dp[0][j] = j;
+    }
+
+    //filling the 2D array
+    for(int i = 1; i<=len1; i++){
+        for(int j = 1; j<=len2; j++){
+            int cost = (word1[i-1] == word2[j-1]) ? 0 : 1; //if the characters are the same, cost is 0, otherwise 1
+            //get the minimum of the three possible operations
+            //insert - dp[i][j-1] + 1
+            //delete - dp[i-1][j] + 1
+            //replace - dp[i-1][j-1] + cost
+            dp[i][j] = dp[i-1][j-1] + cost;
+            if(dp[i][j] > dp[i][j-1] + 1){
+                dp[i][j] = dp[i][j-1] + 1;
+            }else if(dp[i][j] > dp[i-1][j] + 1){
+                dp[i][j] = dp[i-1][j] + 1;
+            }
+        }
+    }
+    printf("%d\n", dp[len1][len2]);
 }
