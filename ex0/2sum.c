114245b637e37d317520b850f2fa569c74144083
tom9798 <soustieltom@gmail.com>
2024-11-15 18:46:47 +0200

    with sort 2sum

--- ex0/2sum.c
+++ ex0/2sum.c
@@ -20,13 +20,36 @@ int main() {
 }
 
 void twoSum(int nums[], int nums_size, int target) {
+//    for (int i = 0; i < nums_size; i++) {
+//        for (int j = i + 1; j < nums_size; j++) {
+//            if (nums[i] + nums[j] == target) {
+//                printf("(%d, %d)\n", i, j);
+//                return;
+//            }
+//        }
+//    }
+    //preform a sort on the array
+    int temp;
     for (int i = 0; i < nums_size; i++) {
         for (int j = i + 1; j < nums_size; j++) {
-            if (nums[i] + nums[j] == target) {
-                printf("(%d, %d)\n", i, j);
-                return;
+            if (nums[i] > nums[j]) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
         }
     }
-
+    //find the two numbers that add up to the target
+    int left = 0;
+    int right = nums_size - 1;
+    while (left < right) {
+        if (nums[left] + nums[right] == target) {
+            printf("(%d, %d)\n", left, right);
+            return;
+        } else if (nums[left] + nums[right] < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
 }
