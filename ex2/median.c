d46a140e7c7f070786de557a9bfa81e382b233ae
tom9798 <soustieltom@gmail.com>
2024-11-29 17:56:35 +0200

    may work, no inoput option

--- ex2/median.c
+++ ex2/median.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void openGradesFile(char *path) {
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        printf("Error: Unable to open file\n");
+int main (){
+    FILE *grades;
+    grades = fopen("grades.txt", "r");
+    if (grades == NULL){
+        printf("Error opening file\n");
         exit(1);
     }
-    printf("File opened successfully\n");
-    fclose(file);
-}
-
-int main() {
-    char path[100];
-    scanf("%s", path);
-    openGradesFile(path);
+    int grade;
+    int sum = 0;
+    int count = 0;
+    while (fscanf(grades, "%d", &grade) != EOF){
+        sum += grade;
+        count++;
+    }
+    printf("Average: %.2f\n", (float)sum/count);
+    fclose(grades);
     return 0;
 }
