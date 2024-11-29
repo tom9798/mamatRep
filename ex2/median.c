17be70604bf321c9c3b4997c6a7fb7887eef024e
tom9798 <soustieltom@gmail.com>
2024-11-29 18:29:36 +0200

    accidently calculated average, now its median

--- ex2/median.c
+++ ex2/median.c
@@ -10,14 +10,41 @@ int main (){
         printf("Error opening file\n");
         exit(1);
     }
-    int grade;
-    int sum = 0;
-    int count = 0;
-    while (fscanf(grades, "%d", &grade) != EOF){
-        sum += grade;
-        count++;
+    //perform a radix sort on the grades
+    int gradesArray[1000];
+    int gradesCount = 0;
+    while (fscanf(grades, "%d", &gradesArray[gradesCount]) != EOF){
+        gradesCount++;
     }
-    printf("Average: %.2f\n", (float)sum/count);
+    //perform a radix sort on the grades
+    int max = gradesArray[0];
+    for (int i = 1; i < gradesCount; i++){
+        if (gradesArray[i] > max){
+            max = gradesArray[i];
+        }
+    }
+    int exp = 1;
+    int *output = (int *)malloc(gradesCount * sizeof(int));
+    while (max / exp > 0){
+        int count[10] = {0};
+        for (int i = 0; i < gradesCount; i++){
+            count[(gradesArray[i] / exp) % 10]++;
+        }
+        for (int i = 1; i < 10; i++){
+            count[i] += count[i - 1];
+        }
+        for (int i = gradesCount - 1; i >= 0; i--){
+            output[count[(gradesArray[i] / exp) % 10] - 1] = gradesArray[i];
+            count[(gradesArray[i] / exp) % 10]--;
+        }
+        for (int i = 0; i < gradesCount; i++){
+            gradesArray[i] = output[i];
+        }
+        exp *= 10;
+    }
+    //find the median
+    if (gradesCount % 2 == 0){
+        printf("%d\n", (gradesArray[gradesCount / 2 - 1] + gradesArray[gradesCount / 2]) / 2);
     fclose(grades);
     return 0;
 }
