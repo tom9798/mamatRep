34df02f4eeae6e12895bf75305c1eb1da673e55f
tom9798 <soustieltom@gmail.com>
2024-12-04 10:59:21 +0200

    now sorting correctly

--- ex1/alerts_hist.sh
+++ ex1/alerts_hist.sh
@@ -1,14 +1,1 @@
 #!/bin/bash
-today=$2
-json_data=$(awk -F, '{print$1}' $1)
-declare -A city_counts
-echo $json_data
-while IFS= read -r city; do
-    if [[ -n "$city" ]]; then
-	city="${city//\'/\\\'}"
-        ((city_counts["$city"]++))
-    fi
-done < <(echo "$json_data" | grep -oP '{[^}]*}' | grep "$today" | grep -oP '"data": *\K"[^"]*"' | tr -d '"')
-for city in "${!city_counts[@]}"; do
-    echo "${city_counts[$city]} $city"
-done | sort -nr
