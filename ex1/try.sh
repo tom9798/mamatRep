7b392d3be8ca23ef9a41e903c1c1f066c50b7b25
tom9798 <soustieltom@gmail.com>
2024-12-04 08:50:03 +0200

    it works ant its shorter

--- ex1/try.sh
+++ ex1/try.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-today=$2
-sed 's#},{#\n#g' $1 | grep $today | awk -F, '{print$1}' | awk -F: '{print$2}' | sed 's#"##g' | uniq -c | sort -nr
+sed 's#},{#\n#g' $1 | grep $2 | awk -F, '{print$1}' | awk -F: '{print$2}' | sed 's#"##g' | uniq -c | sort -nr
