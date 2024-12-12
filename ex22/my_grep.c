9edf92d6ba78aaad820fcdbf49c74db432ff2bca
tom9798 <soustieltom@gmail.com>
2024-12-12 15:12:00 +0200

    something is wrong with the fgets func i think

--- ex22/my_grep.c
+++ ex22/my_grep.c
@@ -6,10 +6,11 @@ void my_grep(FILE *f, char *pattern){
     int line_number = 0;
     int check = 0;
     switch (pattern[0]) {
-        case '^': check = 1;
-        case '$': check = 2;
-        case '.': check = 3;
-        case '*': check = 4;
+        case '^': check = 1; break; 
+        case '$': check = 2; break;
+        case '.': check = 3; break;
+        case '*': check = 4; break;
+	default: check =0; break;
     }
     while(fgets(line, 1000, f) != NULL){
         line_number++;
@@ -18,7 +19,7 @@ void my_grep(FILE *f, char *pattern){
                 break;
             }
             //check if the pattern is at the end of the line
-            if (check == 1 & line[i] == pattern[1]) {
+            if (check == 1 && line[i] == pattern[1]) {
                 int j = 1;
                 while (pattern[j] != '\0') {
                     if (line[i + j] != pattern[j]) {
@@ -31,7 +32,7 @@ void my_grep(FILE *f, char *pattern){
                     return;
                 }
                 //check if the pattern is at the beginning of the line
-            } else if (check == 2 & line[i] == pattern[1]) {
+            } else if (check == 2 && line[i] == pattern[1]) {
                 int j = 1;
                 while (pattern[j] != '\0') {
                     if (line[i + j] != pattern[j]) {
@@ -44,11 +45,11 @@ void my_grep(FILE *f, char *pattern){
                     return;
                 }
             //check if a char is anywhere in the line
-            } else if (check == 3 & line[i] == pattern[1]) {
+            } else if (check == 3 && line[i] == pattern[1]) {
                 printf("Line %d: %s", line_number, line);
                 return;
             //check if the previous char is in the line 0 or more times
-            } else if (check == 4 & line[i] == pattern[1]) {
+            } else if (check == 4 && line[i] == pattern[1]) {
                 int j = 1;
                 while (pattern[j] != '\0') {
                     if (line[i + j] != pattern[j]) {
