5f512fb42c3a6fea5883839ee7201f0c703756ac
tom9798 <soustieltom@gmail.com>
2025-01-21 22:03:33 +0200

    completed phase 3

--- ex9/phase_3.c
+++ ex9/phase_3.c
@@ -1,7 +1,7 @@
 void phase_3(char *input) {
     int index, value;
-    int array[] = {0x171, 0xBC, 0x279, 0x41, 0x41, 0x41, 0x41, 0x41};
-    // array = {369, -188, 633, 65, 65, 65, 65, 65}
+    int array[] = {749, 380, 568, -65, 0, -65};
+    // array = {369, -188, 633, -65, +65, -65}
 
     if (sscanf(input, "%d %d", &index, &value) <= 1) {
         explode_bomb();
