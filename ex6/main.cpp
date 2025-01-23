5cdd3aff03e272bf85904506688a35f2839a9f71
tom9798 <soustieltom@gmail.com>
2025-01-23 16:38:58 +0200

    done with MakfFile and main

--- ex6/main.cpp
+++ ex6/main.cpp
@@ -1,56 +1,36 @@
-#include "string.h"
 #include <iostream>
+#include "string.h"
+#include "string-array.h"
+#include "port.h"
+#include "ip.h"
+#include "input.h"
 
-int main() {
-    // Test constructor
-    String str1("Hello, World!");
-    std::cout << "str1: " << str1.get_str() << std::endl;
-
-    // Test copy constructor
-    String str2(str1);
-    std::cout << "str2 (copy of str1): " << str2.get_str() << std::endl;
-
-    // Test operator=
-    str2 = "Goodbye, World!";
-    std::cout << "str2 (after assignment): " << str2.get_str() << std::endl;
-
-    // Test operator== with GenericString
-    String str3("Goodbye, World!");
-    std::cout << "str2 == str3: " << (str2 == str3) << std::endl;
-
-    // Test operator== with char*
-    std::cout << "str2 == \"Goodbye, World!\": " << (str2 == "Goodbye, World!") << std::endl;
-
-    // Test to_integer
-    String str4("12345");
-    std::cout << "str4 as integer: " << str4.to_integer() << std::endl;
-
-    // Test trim
-    String str5("   Trim me!   "); //#####################need to fix###################
-    str5.trim();
-    std::cout << "str5 (after trim): \"" << str5.get_str() << "\"" << std::endl;
-
-    // Test split
-    String str6("split,this,string");
-    StringArray array = str6.split(",");
-    std::cout << "str6 split by ',':" << std::endl;
-    for (size_t i = 0; i < array.size; ++i) {
-        std::cout << "  " << array.array[i]->as_string().get_str() << std::endl;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input>" << std::endl;
+        return 1;
     }
 
-    // Test split with multiple delimiters
-    String str7("split,this,string too");
-    StringArray array2 = str7.split(", ");
-    std::cout << "str7 split by ', ':" << std::endl;
-    for (size_t i = 0; i < array2.size; ++i) {
-        std::cout << "  " << array2.array[i]->as_string().get_str() << std::endl;
+    String input(argv[1]);
+    StringArray rule = input.split("=");
+
+    if (rule.getSize() < 2) {
+        std::cerr << "Invalid input format: " << std::endl;
+        return 1;
     }
 
-    // Clean up
-    for (size_t i = 0; i < array.size; ++i) {
-        delete array.array[i];
-    }
-    delete[] array.array;
+    String name = rule[0].trim().as_string();
+    String val = rule[1].trim().as_string();
 
+    if (name == "src-ip" || name == "dst-ip") {
+        ip main_ip(name);
+        main_ip.set_value(val);
+        parse_input(main_ip);
+    }
+    else if (name == "src-port" || name == "dst-port") {
+        port main_port(name);
+        main_port.set_value(val);
+        parse_input(main_port);
+    }
     return 0;
 }
