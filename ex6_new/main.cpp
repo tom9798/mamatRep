dc7942068700474a8994097caed3693e6e952d82
tom9798 <soustieltom@gmail.com>
2025-01-27 19:18:14 +0200

    fixed ip cpp

--- ex6_new/main.cpp
+++ ex6_new/main.cpp
@@ -5,20 +5,20 @@
 #include "input.h"
 
 int main(int argc,char* argv[]){
-    String *Rule = make_string(argv[1]);
+    GenericString *Rule = make_string(argv[1]);
     StringArray Type_Rule = Rule->split("=");
     Type_Rule.trimArray();
-    String *type = Type_Rule.stringAtIndex(0)->as_string();
+    String type = Type_Rule.stringAtIndex(0)->as_string();
 
     //port
-    if (*type == "src-port" || *type == "dst-port"){
+    if (type == "src-port" || type == "dst-port"){
         Port PortRule;
         PortRule.get_rule(*Rule);
         parse_input(PortRule);
     }
 
     //ip
-    else if (*type == "src-ip" || *type == "dst-ip") {
+    else if (type == "src-ip" || type == "dst-ip") {
         Ip IpRule;
         IpRule.get_rule(*Rule);
         parse_input(IpRule);
