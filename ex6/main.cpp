64e819a09f7a9ff52e7c19cc5b545e54320d674f
tom9798 <soustieltom@gmail.com>
2025-01-27 14:11:32 +0200

    trying to compile new ver of ex6

--- ex6/main.cpp
+++ ex6/main.cpp
@@ -1,31 +1,65 @@
+#include <iostream>
+#include "port.h"
 #include "string.h"
 #include "ip.h"
-#include "port.h"
 #include "input.h"
-#include <iostream>
 
+using string::operator==;
+
+//#define IP_RULE "ip"
+//#define PORT_RULE "port"
 int main(int argc,char* argv[]){
-    // check if ip or port
-    GenericString *rule = make_string(argv[1]);
-    StringArray ipORport = rule->split("=");
-    bool valid_input = false; // true when valid input is approved
-    // check if ip or port
-    if(strcmp(ipORport.array[0]->as_string()->str,"ip-src") == 0 || strcmp(ipORport.array[0]->as_string()->str,"ip-dst") == 0) {
-        valid_input = true;
-        Ip ip_rule;
-        ip_rule.edit_rule(*rule);
-        parse_input(ip_rule);
+    GenericString *Rule = (argv[1]);
+    StringArray Type-Rule = Rule->split("=");
+    Type-Rule.string_array_trim();
+    String *type = Type-Rule.stringAtIndex(0)->as_string();
+
+    //port
+    if (*type == "src-port" || *type == "dst-port"){
+        Port PortRule;
+        PortRule.get_rule(*Rule);
+        PortRule(port_rule);
     }
-    if(strcmp(ipORport.array[0]->as_string()->str,"port-src") == 0 || strcmp(ipORport.array[0]->as_string()->str,"port-dst") == 0){
-        valid_input = true;
-        Port port_rule;
-        port_rule.edit_rule(*rule);
-        parse_input(port_rule);
-    }
-    delete rule;
-    if(!valid_input){
-        cerr << "Invalid input \n";
-        return 1;
-    }
-    return 0;
 }
+
+
+//// check if ip or port
+//GenericString *rule = make_string(argv[1]);
+//StringArray rule_type = rule->split("-.=");
+//rule_type.string_array_trim();
+//bool valid_input = false; // true when valid input is approved
+//for (int i = 0; i < rule_type.get_size(); i++) {
+//// check if ip
+//if(strcmp(rule_type.get_string_from_array(i)->as_string().get_str(),IP_RULE) == 0) {
+//valid_input = true;
+//Ip ip_rule;
+//ip_rule.edit_rule(*rule);
+//parse_input(ip_rule);
+//break;
+//}
+//// check if port
+//if(strcmp(rule_type.get_string_from_array(i)->as_string().get_str(),PORT_RULE) == 0){
+//valid_input = true;
+//Port port_rule;
+//port_rule.edit_rule(*rule);
+//parse_input(port_rule);
+//break;
+//}
+//}
+//delete rule;
+//if(!valid_input){
+//cerr << "Invalid inputaa \n";
+//return 1;
+//}
+//return 0;
+
+
+
+
+
+
+
+
+
+
+
---
+++ ex6_new/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "port.h"
+#include "string.h"
+#include "ip.h"
+#include "input.h"
+
+int main(int argc,char* argv[]){
+    GenericString *Rule = (argv[1]);
+    StringArray Type-Rule = Rule->split("=");
+    Type-Rule.string_array_trim();
+    String *type = Type-Rule.stringAtIndex(0)->as_string();
+
+    //port
+    if (*type == "src-port" || *type == "dst-port"){
+        Port PortRule;
+        PortRule.get_rule(*Rule);
+        parse_input(PortRule);
+    }
+
+    //ip
+    else if (*type == "src-ip" || *type == "dst-ip") {
+        Ip IpRule;
+        IpRule.get_rule(*Rule);
+        parse_input(IpRule);
+    }
+
+    delete Rule;
+}
+
+
+//// check if ip or port
+//GenericString *rule = make_string(argv[1]);
+//StringArray rule_type = rule->split("-.=");
+//rule_type.string_array_trim();
+//bool valid_input = false; // true when valid input is approved
+//for (int i = 0; i < rule_type.get_size(); i++) {
+//// check if ip
+//if(strcmp(rule_type.get_string_from_array(i)->as_string().get_str(),IP_RULE) == 0) {
+//valid_input = true;
+//Ip ip_rule;
+//ip_rule.edit_rule(*rule);
+//parse_input(ip_rule);
+//break;
+//}
+//// check if port
+//if(strcmp(rule_type.get_string_from_array(i)->as_string().get_str(),PORT_RULE) == 0){
+//valid_input = true;
+//Port port_rule;
+//port_rule.edit_rule(*rule);
+//parse_input(port_rule);
+//break;
+//}
+//}
+//delete rule;
+//if(!valid_input){
+//cerr << "Invalid inputaa \n";
+//return 1;
+//}
+//return 0;
