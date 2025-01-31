0559a662c8983c3da1cfde13e353c1cdadf0fc38
tom9798 <soustieltom@gmail.com>
2025-01-31 14:02:22 +0200

    solved phase_6

--- ex6_new/firewall.sh
+++ ex6_new/firewall.sh
@@ -6,26 +6,26 @@ allRules=$(<$1)
 
 # Remove comments and empty lines
 allRules=$(echo "$allRules" | sed -e 's/#.*//' -e '/^$/d')
-echo "$allRules"
+#echo "$allRules"
 
 # Loop over all rules
 while IFS=, read -r src_ip dst_ip src_port dst_port; do
     # Remove spaces
     src_ip=$(echo "$src_ip" | tr -d ' ')
-    echo "$src_ip"
+#    echo "$src_ip"
     dst_ip=$(echo "$dst_ip" | tr -d ' ')
-    echo "$dst_ip"
+#    echo "$dst_ip"
     src_port=$(echo "$src_port" | tr -d ' ')
-    echo "$src_port"
+#    echo "$src_port"
     dst_port=$(echo "$dst_port" | tr -d ' ')
-    echo "$dst_port"
+#    echo "$dst_port"
 
     # Run the packets through the firewall for each rule
     passedPackets=$(echo "$allPackets" | ./firewall.exe "$src_ip" | \
                     ./firewall.exe "$dst_ip" | \
                     ./firewall.exe "$src_port" | \
                     ./firewall.exe "$dst_port")
-
+    echo "$passedPackets"
     # Append the packets that passed to the final result
     allPassedPackets+="$passedPackets"$'\n'
 
