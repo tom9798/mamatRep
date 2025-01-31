2db0426f47db6f126b19c5151be812373d2b4fda
tom9798 <soustieltom@gmail.com>
2025-01-31 15:04:03 +0200

    solved phase_6

--- ex6_new/firewall.sh
+++ ex6_new/firewall.sh
@@ -13,19 +13,19 @@ all_rules=$(echo "$all_rules" | awk '{sub(/#.*/, "", $0); if (NF) print}')
 while IFS= read -r rule; do
     iter_rule="$rule"
     # split to src-ip, dst-ip, src-port, dst-port
-    IFS=',' read -r src_ip dst_ip src_port dst_port <<< "$iter_rule"
-    src_ip=${src_ip// /}
-    dst_ip=${dst_ip// /}
-    src_port=${src_port// /}
-    dst_port=${dst_port// /}
+    IFS=',' read -r rule1 rule2 rule3 rule4 <<< "$iter_rule"
+    rule1=${rule1// /}
+    rule2=${rule2// /}
+    rule3=${rule3// /}
+    rule4=${rule4// /}
 
     # process packets with each rule separately
-    out=$(echo "$packets" | ./firewall.exe "$src_ip" | ./firewall.exe "$dst_ip" | ./firewall.exe "$src_port" | ./firewall.exe "$dst_port" | sort | uniq)
+    out=$(echo "$packets" | ./firewall.exe "$rule1" | ./firewall.exe "$rule2" | ./firewall.exe "$rule3" | ./firewall.exe "$rule4" | sort | uniq)
 
     packets_passed+=" $out"
 
     # clean up
-    unset src_ip dst_ip src_port dst_port out
+    unset rule1 rule2 rule3 rule4 out
 
 done <<< "$all_rules"
 
