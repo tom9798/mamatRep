42d4f0544f2735d2428bdcd935f6bddfde0957ac
tom9798 <soustieltom@gmail.com>
2025-01-31 14:30:27 +0200

    solved phase_6

--- ex6_new/firewall.sh
+++ ex6_new/firewall.sh
@@ -1,31 +1,71 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
-allPackets=$(cat)
-#allRules=$(<$1)
-Rules="$1"
+packets=$(</dev/stdin)
+rules_file=$1
+all_rules=$(<$rules_file)
 
-# Remove comments and empty lines
-allRules=$(sed -e 's/#.*//' -e '/^$/d' "$Rules")
+# keep packets that passed:
+packets_passed=""
+# clean comments and empty lines:
+all_rules=$(echo "$all_rules" | awk '{sub(/#.*/, "", $0); if (NF) print}')
 
-# Loop over all rules
-while IFS=, read -r src_ip dst_ip src_port dst_port; do
-#    src_ip=$(echo "$src_ip" | tr -d ' ')
-#    dst_ip=$(echo "$dst_ip" | tr -d ' ')
-#    src_port=$(echo "$src_port" | tr -d ' ')
-#    dst_port=$(echo "$dst_port" | tr -d ' ')
-    # Run the packets through the firewall for each rule
-    passedPackets=$(echo "$allPackets" | ./firewall.exe "$src_ip" | \
-                    ./firewall.exe "$dst_ip" | \
-                    ./firewall.exe "$src_port" | \
-                    ./firewall.exe "$dst_port")
-    allPassedPackets+="$passedPackets"$'\n'
-done <<< "$allRules"
-#remove spaces and empty lines and sort the packets
-#allPassedPackets=$(echo "$allPassedPackets" | tr -d ' ' | sed '/^$/d' | sort -u)
-allPassedPackets$="${allPassedPackets// /}"
-allPassedPackets=$(echo -e "$allPassedPackets" | sed "s/ //g ; /^$/d" | sort -u)
-echo "$allPassedPackets"
+while IFS= read -r rule; do
+    iter_rule="$rule"
+    # split to src-ip, dst-ip, src-port, dst-port
+    rule1=$(echo "$iter_rule" | cut -d',' -f1 | tr -d ' ')
+    rule2=$(echo "$iter_rule" | cut -d',' -f2 | tr -d ' ')
+    rule3=$(echo "$iter_rule" | cut -d',' -f3 | tr -d ' ')
+    rule4=$(echo "$iter_rule" | cut -d',' -f4 | tr -d ' ')
+	# insert packets file to run with each rule part while passing the output to the next rule
+   	out=$(echo "$packets" | ./firewall.exe "$rule1" | sort | tr -d ' ')
+   	out=$(echo "$out" | ./firewall.exe "$rule2" | sort | tr -d ' ')
+   	out=$(echo "$out" | ./firewall.exe "$rule3" | sort | tr -d ' ')
+   	out=$(echo "$out" | ./firewall.exe "$rule4" | sort | tr -d ' ')
+
+   	tmp_passed=$(echo "$out" | sort | uniq -c | grep -E "^\s*4\s+" | awk '{print $2}')
+   	packets_passed+=" $tmp_passed"
+
+    # clean up
+    unset rule1 rule2 rule3 rule4 out tmp_passed
+
+done < <(echo "$all_rules" | awk '{sub(/#.*/, "", $0); print}' | awk 'NF')
+
+packets_passed=$(echo "$packets_passed"| tr ' ' '\n' | awk 'NF' | sort | uniq)
+echo "$packets_passed"
+
+
+
+
+
+
+
+
+
+
+##!/bin/bash
+#export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+#
+#allPackets=$(cat)
+##allRules=$(<$1)
+#Rules="$1"
+#
+## Remove comments and empty lines
+#allRules=$(sed -e 's/#.*//' -e '/^$/d' "$Rules")
+#
+## Loop over all rules
+#while IFS=, read -r src_ip dst_ip src_port dst_port; do
+#    passedPackets=$(echo "$allPackets" | ./firewall.exe "$src_ip" | \
+#                    ./firewall.exe "$dst_ip" | \
+#                    ./firewall.exe "$src_port" | \
+#                    ./firewall.exe "$dst_port")
+#    allPassedPackets+="$passedPackets"$'\n'
+#done <<< "$allRules"
+##remove spaces and empty lines and sort the packets
+##allPassedPackets=$(echo "$allPassedPackets" | tr -d ' ' | sed '/^$/d' | sort -u)
+#allPassedPackets$="${allPassedPackets// /}"
+#allPassedPackets=$(echo -e "$allPassedPackets" | sed "s/ //g ; /^$/d" | sort -u)
+#echo "$allPassedPackets"
 
 
 
